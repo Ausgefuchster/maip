14d8f050f436f17c4f28dac672d20275e34725d5
Ausgefuchster <51484658+Ausgefuchster@users.noreply.github.com>
2023-06-05 18:06:49 +0200

    feat(policy): add ConditionStatement, docs & use rename_all of serde

--- src/policy.rs
+++ src/policy.rs
@@ -3,11 +3,10 @@ use serde::{Deserialize, Serialize};
 use crate::json_string_or_vec::string_or_seq_string;
 
 #[derive(Serialize, Deserialize, Debug)]
+#[serde(rename_all = "PascalCase")]
 pub struct PolicyDocument {
-    #[serde(alias = "Version")]
     version: String,
-    #[serde(alias = "Statement")]
-    pub statement: Vec<PolicyStatement>,
+    statement: Vec<PolicyStatement>,
 }
 
 impl PolicyDocument {
@@ -16,27 +15,51 @@ impl PolicyDocument {
     }
 }
 
-#[derive(Serialize, Deserialize, Debug, PartialEq)]
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+#[serde(rename_all = "PascalCase")]
 pub struct PolicyStatement {
-    #[serde(alias = "Effect")]
     effect: String,
-    #[serde(alias = "Action", deserialize_with = "string_or_seq_string")]
+
+    #[serde(deserialize_with = "string_or_seq_string")]
     action: Vec<String>,
-    #[serde(alias = "Resource", deserialize_with = "string_or_seq_string")]
+
+    #[serde(deserialize_with = "string_or_seq_string")]
     resource: Vec<String>,
+
+    #[serde(skip_serializing_if = "Option::is_none")]
+    condition: Option<ConditionStatement>,
 }
 
 impl PolicyStatement {
-    pub fn new(effect: String, action: Vec<String>, resource: Vec<String>) -> Self {
+    pub fn new(
+        effect: String,
+        action: Vec<String>,
+        resource: Vec<String>,
+        condition: Option<ConditionStatement>,
+    ) -> Self {
         Self {
             effect,
             action,
             resource,
+            condition,
         }
     }
 }
 
-trait Merge<T> {
+#[derive(Serialize, Deserialize, Debug, PartialEq, Clone)]
+pub struct ConditionStatement {}
+
+pub trait Merge<T> {
+    /// Merges the other value into self
+    /// # Example
+    /// ```
+    /// use maip::policy::Merge;
+    ///
+    /// let mut a: Vec<String> = vec!["a".to_string(), "b".to_string()];
+    /// let b = vec!["b".to_string(), "c".to_string()];
+    /// a.merge(b);
+    /// assert_eq!(a, vec!["a".to_string(), "b".to_string(), "c".to_string()]);
+    /// ```
     fn merge(&mut self, other: T);
 }
 
@@ -51,13 +74,22 @@ impl Merge<Vec<String>> for Vec<String> {
 }
 
 /// Merges the two statements together
+/// - If one statement has a condition, it will return None
 /// - If they have a different effect, but action and resource are the same,
 ///   return a deny statement because deny always overrides allow
-/// - If they have the same effect, merge the action and resource
+/// - Otherwise, If they have the same action it will merge the resources.
+///   Furthermore, if one list contains an asterisk, it will return a statement with
+///   only an asterisk as resource.
+/// - But if they have different actions, it will only merge the resources if they
+///   are equal
 pub fn merge_statements(
     first_statement: &PolicyStatement,
     second_statement: &PolicyStatement,
 ) -> Option<PolicyStatement> {
+    if first_statement.condition.is_some() || second_statement.condition.is_some() {
+        return None;
+    }
+
     if first_statement.effect != second_statement.effect {
         if same_action_and_resource(first_statement, second_statement) {
             return as_deny_statement(first_statement);
@@ -65,21 +97,30 @@ pub fn merge_statements(
         return None;
     }
 
-    let mut merged_statement = PolicyStatement::new(
-        first_statement.effect.clone(),
-        first_statement.action.clone(),
-        first_statement.resource.clone(),
-    );
+    let mut merged_statement = first_statement.clone();
 
-    merged_statement
-        .action
-        .merge(second_statement.action.clone());
+    let first_resource = &first_statement.resource;
+    let second_resource = &second_statement.resource;
 
-    merged_statement
-        .resource
-        .merge(second_statement.resource.clone());
+    let first_action = &first_statement.action;
+    let second_action = &second_statement.action;
 
-    Some(merged_statement)
+    let asterik = get_asterisk(first_resource, second_resource);
+    if first_action == second_action {
+        if let Some(asterisk) = asterik {
+            merged_statement.resource = vec![asterisk];
+            return Some(merged_statement);
+        }
+        merged_statement.resource.merge(second_resource.clone());
+        return Some(merged_statement);
+    }
+
+    if can_merge_resources(first_resource, second_resource) {
+        merged_statement.action.merge(second_action.clone());
+        merged_statement.resource.merge(second_resource.clone());
+        return Some(merged_statement);
+    }
+    None
 }
 
 fn same_action_and_resource(
@@ -95,5 +136,20 @@ fn as_deny_statement(statement: &PolicyStatement) -> Option<PolicyStatement> {
         "Deny".to_string(),
         statement.action.clone(),
         statement.resource.clone(),
+        statement.condition.clone(),
     ))
+}
+
+fn can_merge_resources(first_resources: &[String], second_resources: &[String]) -> bool {
+    first_resources == second_resources || get_asterisk(first_resources, second_resources).is_some()
+}
+
+fn get_asterisk(first_resources: &[String], second_resources: &[String]) -> Option<String> {
+    if first_resources.contains(&"*".to_string()) {
+        return Some("*".to_string());
+    }
+    if second_resources.contains(&"*".to_string()) {
+        return Some("*".to_string());
+    }
+    None
 }
